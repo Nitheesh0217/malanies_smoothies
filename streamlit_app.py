f306f219bcfc506b189b7c0be623cb97875ad3e2
Nitheesh Donepudi <166969412+Nitheesh0217@users.noreply.github.com>
2025-08-08 03:25:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ pd_df = my_dataframe.to_pandas()
 
 # Preview the pandas df
 st.dataframe(pd_df, use_container_width=True)
-st.stop()
 # Instruction for selecting fruits
 st.write("Choose up to 5 fruits for your custom smoothie:")
 
@@ -57,16 +56,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"].iloc[0]
-        st.write(f"The search value for {fruit_chosen} is {search_on}.")
-        st.subheader(fruit_chosen+'Nutrition Information')
-        smoothiefroot_response = requests.get(
-            f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen.lower()}"
-        )
-        sf_df = st.dataframe(
-            data=smoothiefroot_response.json(),
-            use_container_width=True
-        )
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+    # st.write(ingredients_string)
+
 
 
 # ✅ Submit order when ready
